a7c08cf85756fc61e6601a8f7186b03347e0c80a
reubenavetyan <148552144+reubenavetyan@users.noreply.github.com>
2026-08-14 17:07:56 +0400

    Fix template selection logic

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
+import random
 
-
-def hospitalstay():
+def hospital():
     number = input("Input a number: ")
     timemeasure = input("Input an measure of time(hour/min/sec): ")
     transportmode = input("Input a transport mode(car/metro/plane): ")
@@ -14,15 +14,21 @@ def hospitalstay():
     noun2 = input("Input another noun: ")
     noun3 = input("Input one more noun: ")
     bodypart2 = input("Input another body part(head/leg/belly): ")
-    noun3 = input("Input the last noun)): ")
+    noun4 = input("Input the last noun)): ")
     adj3 = input("Input the last adjective: ")
     sillyword = input("Input a silly word: ")
 
-    return print("It was about " + number + " " + timemeasure + " ago when I arrived at the hospital in a "
-                 + transportmode + ". The hospital is a/an " + adj + " place, there are a lot of " + adj2 + " " + noun + " here. There are nurses here who have " + color + " " + bodypart + ". If someone wants to come into my room I told them that they have to " + verb + " first. I’ve decorated my room with " + num2 + " " + noun2 + ". Today I talked to a doctor and they were wearing a " + noun3 + " on their " + bodypart2 + ". I heard that all doctors " + verb + " " + noun3 + " every day for breakfast. The most " + adj3 + " thing about being in the hospital is the " + sillyword + " " + noun + " !")
+    print("It was about " + number + " " + timemeasure + " ago when I arrived at the hospital in a "
+                 + transportmode + ". The hospital is a/an " + adj + " place, there are a lot of " + adj2 + " "
+                 + noun + " here. There are nurses here who have " + color + " " + bodypart
+                 + ". If someone wants to come into my room I told them that they have to "
+                 + verb + " first. I’ve decorated my room with " + num2 + " " + noun2
+                 + ". Today I talked to a doctor and they were wearing a " + noun3 + " on their " + bodypart2
+                 + ". I heard that all doctors " + verb + " " + noun4 + " every day for breakfast. The most "
+                 + adj3 + " thing about being in the hospital is the " + sillyword + " " + noun + " !")
 
 
-def campingtrip():
+def camping():
     name = input("Input Person's name: ")
     noun = input("Input a proper noun: ")
     adj = input("Input a feeling describing adjective: ")
@@ -38,10 +44,16 @@ def campingtrip():
     sillyword = input("Input a silly word: ")
     noun2 = input("Input another noun: ")
 
-    return print("This weekend I am going camping with " + name + ". I packed my lantern, sleeping bag, and " + noun + ". I am so " + adj + " to " + verb + " in a tent. I am " + adj2 + " we might see a(n) " + animal + ", I hear they’re kind of dangerous. While we’re camping, we are going to hike, fish, and " + verb2 + ". I have heard that the " + color + " lake is great for " + verbing + ". Then we will " + adverb + " hike through the forest for " + number + " " + timemeasure + ". If I see a " + color + " " + animal + " while hiking, I am going to bring it home as a pet! At night we will tell " + number + " " + sillyword + " stories and roast " + noun2 + " around the campfire!!!")
+    print("This weekend I am going camping with " + name + ". I packed my lantern, sleeping bag, and " + noun
+                 + ". I am so " + adj + " to " + verb + " in a tent. I am " + adj2 + " we might see a(n) " + animal
+                 + ", I hear they’re kind of dangerous. While we’re camping, we are going to hike, fish, and " + verb2
+                 + ". I have heard that the " + color + " lake is great for " + verbing + ". Then we will " + adverb
+                 + " hike through the forest for " + number + " " + timemeasure + ". If I see a " + color + " " + animal
+                 + " while hiking, I am going to bring it home as a pet! At night we will tell " + number + " " + sillyword
+                 + " stories and roast " + noun2 + " around the campfire!!!")
 
 
-def enchantedcastle():
+def castle():
     name = input("Input Person's name: ")
     adj = input("Input an adjective: ")
     color = input("Input a color: ")
@@ -63,16 +75,41 @@ def enchantedcastle():
     adj5 = input("Input the last adjective: ")
     noun5 = input("Input the last noun: ")
 
-    return print("Dear " + name + ", I am writing to you from a " + adj + " castle in an enchanted forest. I found myself here one day after going for a ride on a " + color + " " + animal + " in " + place + ". There are " + adj2 + " " + magicalcreature + " and " + adj3 + " " + magicalcreature2 + " here! In the " + room + " there is a pool full of " + noun + ". I fall asleep each night on a " + noun2 + " of " + noun3 + " and dream of " + adj4 + " " + noun4 + ". It feels as though I have lived here for " + number + " " + timemeasure + ". I hope one day you can visit, although the only way to get here now is " + verbing + " on a " + adj5 + " " + noun5 + "!!!")
+    print("Dear " + name + ", I am writing to you from a " + adj
+                 + " castle in an enchanted forest. I found myself here one day after going for a ride on a "
+                 + color + " " + animal + " in " + place + ". There are " + adj2 + " " + magicalcreature
+                 + " and " + adj3 + " " + magicalcreature2 + " here! In the " + room + " there is a pool full of "
+                 + noun + ". I fall asleep each night on a " + noun2 + " of " + noun3 + " and dream of " + adj4 + " "
+                 + noun4 + ". It feels as though I have lived here for " + number + " " + timemeasure
+                 + ". I hope one day you can visit, although the only way to get here now is " + verbing + " on a "
+                 + adj5 + " " + noun5 + "!!!")
 
 
-print("1 Hospital Stay      2 Camping Trip      3 Enchanted Castle")
-topicNumber = input("Choose one of our three templates using the numbers written in front of them: ")
-
-if topicNumber == 1: hospitalstay()
-if topicNumber == 2: campingtrip()
-else: enchantedcastle()
+topicNumber = input("Pick an option between (1,2,3) or any other number to get a random choice: ")
 
 
+def isinteger(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
 
 
+while not isinteger(topicNumber):
+    topicNumber = input("INPUT AN INTEGER!:) ")
+
+topicNumber = int(topicNumber)
+
+templates = [1, 2, 3]
+if topicNumber == 1:
+    hospital()
+elif topicNumber == 2:
+    camping()
+elif topicNumber == 3:
+    castle()
+else:
+    topicNumber = random.choice(templates)
+    if topicNumber == 1: hospital()
+    elif topicNumber == 2: camping()
+    elif topicNumber == 3: castle()
